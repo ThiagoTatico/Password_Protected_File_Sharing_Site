6d956b1c8d5bd720363814ec5acb2944e5e255e0
Ariun <thiagotatico007@gmail.com>
2022-06-29 16:33:18 -0300

    Add Mongo DB :tada:

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
 import path from 'path';
 
 const app = express();
+
+const upload = multer({ dest: 'uploads' });
+
+mongoose.connect(process.env.DATABASE_URL);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -10,4 +17,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3333, () => console.log('Server ON !!!'));
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('hiii');
+});
+
+app.listen(process.env.PORT, () => console.log('Server ON !!!'));
