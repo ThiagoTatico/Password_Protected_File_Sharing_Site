e72289310a948d7ca9986981b6f4c011b01ce4b2
Ariun <thiagotatico007@gmail.com>
2022-06-29 15:26:43 -0300

    Initial Configs :tada:

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(3333, () => console.log('Server ON !!!'));
